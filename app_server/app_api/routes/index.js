1c73fbfbac79ce93f38ba02948f0de97cbd50a53
B. Isaac Medina <b3medina@gmail.com>
2024-06-11 16:41:20 -0700

    Module 5 completed baseline

--- app_server/app_api/routes/index.js
+++ app_server/app_api/routes/index.js
@@ -6,7 +6,11 @@ const tripsController = require('../controllers/trips');
 
 
 router
-    .route('trips')
+    .route('/trips')
     .get(tripsController.tripsList);
 
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
+    
 module.exports = router;
